97f448d3616884a3ced98b8def8a4b6607ddd9c3
LeePaulison <lee.paulison.jr@gmail.com>
2024-11-24 21:28:03 -0500

    main is now responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang='en'>
       <body className='bg-stone-100 text-stone-900 font-sans flex'>
         <Sidebar categories={categories} />
-        <main className='flex-1 p-8 overflow-auto'>
+        <main className='flex-1 max-w-screen-lg mx-auto px-4 sm:px-6 lg:px-8 py-8 overflow-auto'>
           <Breadcrumbs />
           {children}
         </main>
